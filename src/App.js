5e3f8021e2bc71d802a3aee535a1d590aab82cb0
Zac Smith <zrs3141592@gmail.com>
2020-03-31 21:36:53 -0600

    use ternary to display correct format

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-
 import './App.css';
+import Intro from './components/Intro.js'
 
 const getCurrentDate = () => {
   const date = new Date()
   return date.toDateString()
 }
 
+const getCurrentTime = () => {
+  const time = new Date()
+  const hour = time.getHours()
+  const mins = time.getMinutes()
+  const sec = time.getSeconds()
+
+  const minutes = mins < 10 ? `0${mins}` : `${mins}`
+  const seconds = sec < 10 ? `0${sec}` : `${sec}`
+
+
+  return `${hour}:${minutes}.${seconds}`
+}
+
+
+
 const App = () => {
   return (
     <div className="App">
-      <h1>The current date is: {getCurrentDate()}</h1>
+      <Intro date={getCurrentDate()} time={getCurrentTime()} />
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+const Intro = (props) => <h1>The current date and time is: {props.date} @ {props.time}</h1>
+
+export default Intro
